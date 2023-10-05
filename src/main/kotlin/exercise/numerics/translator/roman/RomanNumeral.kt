00800ea3d0fe331b99d27e393dfd53c9ab628732
Adam Myers <myers.adam.k@gmail.com>
2023-10-05 18:05:41 -0400

    A fun diversion with .fold, but on second thought, not the most efficient way to build a string.

---
+++ src/main/kotlin/exercise/numerics/translator/roman/RomanNumeral.kt
@@ -0,0 +1,5 @@
+package exercise.numerics.translator.roman
+
+enum class RomanNumeral(val value: Int, val symbol: String) {
+    M(1000, "M")
+}
--- src/main/kotlin/exercise/numerics/translator/roman/RomanNumeralTranslator.kt
+++ src/main/kotlin/exercise/numerics/translator/roman/RomanNumeralTranslator.kt
@@ -3,7 +3,18 @@ package exercise.numerics.translator.roman
 import exercise.numerics.translator.NumericTranslator
 
 class RomanNumeralTranslator : NumericTranslator<Int> {
+
     override fun toText(value: Int): String {
-        TODO("Not yet implemented")
+        return ORDERED_NUMERALS.fold("") { accumulator, romanNumeral ->
+            val occurrences = value / romanNumeral.value
+            if (occurrences < 0) {
+               return ""
+            }
+            return accumulator + romanNumeral.symbol.repeat(occurrences)
+        }
+    }
+
+    companion object {
+        val ORDERED_NUMERALS = RomanNumeral.values().sortedByDescending { it.value }
     }
 }
