4941de518d7c6a3e4c5f7d862539ec76d767c6c5
Adam Myers <myers.adam.k@gmail.com>
2023-10-05 17:50:38 -0400

    Add initial failing test

--- src/test/kotlin/exercise/numerics/translator/roman/RomanNumeralTranslatorTest.kt
+++ src/test/kotlin/exercise/numerics/translator/roman/RomanNumeralTranslatorTest.kt
@@ -1,6 +1,14 @@
 package exercise.numerics.translator.roman
 
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.jupiter.api.Test
+
 
 class RomanNumeralTranslatorTest {
+    private val translator: RomanNumeralTranslator = RomanNumeralTranslator()
 
+    @Test
+    fun `Translates 1000 to M`() {
+        assertThat(translator.toText(1000)).isEqualTo("M")
+    }
 }
