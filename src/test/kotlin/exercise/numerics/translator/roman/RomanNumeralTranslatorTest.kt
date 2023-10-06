55121014510cb60a93b24efbcf5aa4bffbdf7b97
Adam Myers <myers.adam.k@gmail.com>
2023-10-06 07:35:32 -0400

    Realizing that 0 and -1 are not valid roman numerals, add tests for these cases. Pull in kotlin.test instead of AssertJ for more idiomatic assertions.

--- src/test/kotlin/exercise/numerics/translator/roman/RomanNumeralTranslatorTest.kt
+++ src/test/kotlin/exercise/numerics/translator/roman/RomanNumeralTranslatorTest.kt
@@ -1,14 +1,33 @@
 package exercise.numerics.translator.roman
 
-import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 
 class RomanNumeralTranslatorTest {
     private val translator: RomanNumeralTranslator = RomanNumeralTranslator()
 
     @Test
+    fun `Rejects zero`() {
+        assertFailsWith(
+            exceptionClass = IllegalAccessException::class,
+            message = "0 is not supported by the roman numeral system",
+            block = { translator.toText(0) }
+        )
+    }
+
+    @Test
+    fun `Rejects negative values`() {
+        assertFailsWith(
+            exceptionClass = IllegalAccessException::class,
+            message = "Negative numbers are not supported by the roman numeral system",
+            block = { translator.toText(-1) }
+        )
+    }
+
+    @Test
     fun `Translates 1000 to M`() {
-        assertThat(translator.toText(1000)).isEqualTo("M")
+        assertEquals("M", translator.toText(1000))
     }
 }
