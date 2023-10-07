0374e35eb34d67007a0a83b6b4080f6316a09019
Adam Myers <myers.adam.k@gmail.com>
2023-10-07 12:48:57 -0400

    Configure from-roman-numeral command, fixing integration test

--- src/main/kotlin/exercise/numerics/translator/config/CommandConfig.kt
+++ src/main/kotlin/exercise/numerics/translator/config/CommandConfig.kt
@@ -8,26 +8,37 @@ import org.springframework.shell.command.CommandRegistration
 import org.springframework.shell.context.InteractionMode
 
 @Configuration
-class CommandConfig {
+class CommandConfig(romanNumeralTranslator: RomanNumeralTranslator) {
+
+    private val command = SimpleTranslatorCommand(
+        key = "roman-numeral",
+        numericType = Int::class.java,
+        translator = romanNumeralTranslator
+    )
 
     @Bean
     fun romanNumerals(translator: RomanNumeralTranslator): CommandRegistration {
-        val command = SimpleTranslatorCommand(
-            key = "roman-numeral",
-            numericType = Int::class.java,
-            translator = translator
-        )
+        return CommandRegistration.builder().interactionMode(InteractionMode.ALL)
+                .command("to-${command.key}")
+                .withTarget()
+                .consumer(command::translateTo).and()
+                .withOption()
+                .longNames(command.argumentName)
+                .required()
+                .type(command.numericType)
+                .and().build()
+    }
 
-        return CommandRegistration.builder()
-            .interactionMode(InteractionMode.ALL)
-            .command("to-${command.key}")
-            .withTarget()
-            .consumer(command::translateTo).and()
-            .withOption()
-            .longNames(command.argumentName)
-            .required()
-            .type(command.numericType)
-            .and()
-            .build()
+    @Bean
+    fun fromRomanNumeral(translator: RomanNumeralTranslator): CommandRegistration {
+        return CommandRegistration.builder().interactionMode(InteractionMode.ALL)
+                .command("from-${command.key}")
+                .withTarget()
+                .consumer(command::translateFrom).and()
+                .withOption()
+                .longNames(command.argumentName)
+                .required()
+                .type(String::class.java)
+                .and().build()
     }
 }
